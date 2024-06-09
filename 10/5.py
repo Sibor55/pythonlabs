32cc03a75d6a7de8293ff57653c4d63aa19dc5e8
Sibor55 <nikita-borisov-2005@mail.ru>
2024-06-09 19:05:24 +0200

    update

--- 10/5.py
+++ 10/5.py
@@ -7,7 +7,7 @@ X = np.linspace(-5, 5, 100)
 Y = np.linspace(-5, 5, 100)
 X, Y = np.meshgrid(X, Y)
 Z = np.sqrt(X**2 + Y**2)  # MSE
-Z2 = np.log(Z)
+# Z2 = np.log(Z)
 # Построение первого трехмерного графика
 fig = plt.figure(figsize=(10, 5))
 ax1 = fig.add_subplot(1, 2, 1, projection='3d')
@@ -19,7 +19,8 @@ ax1.set_zlabel('Z')
 
 # Построение второго трехмерного графика с логарифмическим масштабом по Я
 ax2 = fig.add_subplot(1, 2, 2, projection='3d')
-ax2.plot_surface(X, Y, Z2, cmap='viridis')
+# ax2.plot_surface(X, Y, Z2, cmap='viridis')
+ax2.plot_surface(X, Y, Z, cmap='viridis')
 ax2.set_title('Среднеквадратичное отклонение (логарифмическая ось Z)')
 ax2.set_xlabel('X')
 ax2.set_ylabel('Y')
