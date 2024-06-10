40a195ad21dc61668cbce75165f96207254262d5
Sibor55 <nikita-borisov-2005@mail.ru>
2024-06-10 11:16:39 +0200

    2

--- 9/1.py
+++ 9/1.py
@@ -1,33 +1,16 @@
-# Сохранение текста в файл
-matrix_text = """3,4,17,-3
-5,11,-1,6
-0,2,-5,8"""
+import numpy as np
 
-file_path = '9/matrix.txt'
+matrix = np.array([[3, 4, 17, -3],
+                   [5, 11, -1, 6],
+                   [0, 2, -5, 8]])
 
-with open(file_path, 'w') as file:
-    file.write(matrix_text)
+np.savetxt('9/matrix.txt', matrix, fmt='%d')
 
-matrix = []
+loaded_matrix = np.loadtxt('9/matrix.txt', dtype=int)
 
-with open(file_path, 'r') as file:
-    for line in file:
-        row = [int(num) for num in line.strip().split(',')]
-        matrix.append(row)
 
-# Нахождение суммы, максимального и минимального элементов
-total_sum = 0
-max_element = float('-inf')
-min_element = float('inf')
+sum_elements = np.sum(loaded_matrix)
+max_element = np.max(loaded_matrix)
+min_element = np.min(loaded_matrix)
 
-for row in matrix:
-    for num in row:
-        total_sum += num
-        if num > max_element:
-            max_element = num
-        if num < min_element:
-            min_element = num
-
-print(f"Сумма всех элементов матрицы: {total_sum}")
-print(f"Максимальный элемент матрицы: {max_element}")
-print(f"Минимальный элемент матрицы: {min_element}")
+print(f"Сумма {sum_elements}, Макс элемент {max_element}, Мин элемент {min_element}")
